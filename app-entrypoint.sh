638666192818328b85bc372b145a7028c0fdb6d9
RoyAzran <royazran177@gmail.com>
2025-09-08 15:09:54 +0300

    Update app-entrypoint.sh

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -3,13 +3,9 @@
 set -e
 
 chmod +x ./upgrade.sh
+
 PYTHON=/usr/bin/python3.11 bash upgrade.sh
 
 source venv/bin/activate
-
-export DJANGO_SUPERUSER_USERNAME=kfir
-export DJANGO_SUPERUSER_PASSWORD=roy
-export DJANGO_SUPERUSER_EMAIL=kfir@example.com
-python3 statuspage/manage.py createsuperuser --no-input 
 
 exec python3 statuspage/manage.py runserver 0.0.0.0:8000 --insecure
