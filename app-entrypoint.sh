dd22cfb83d60b1de5f797488c10114f4a163d65e
RoyAzran <royazran177@gmail.com>
2025-09-16 11:47:35 +0300

    Update app-entrypoint.sh

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-
 set -e
 
 chmod +x ./upgrade.sh
 
-PYTHON=/usr/bin/python3.11 bash upgrade.sh
+# Detect Python 3.11 automatically
+PYTHON=$(command -v python3.11) bash upgrade.sh
 
-source venv/bin/activate
+. venv/bin/activate
 
-cd /opt/status-page/statuspage 
+cd /opt/status-page/statuspage
 exec gunicorn statuspage.wsgi:application --bind 0.0.0.0:8000 --workers 3
